0cd9d3e67db20ce8ffb60c5614d6fb4447895dfe
ravencash-net <gandbalest@gmail.com>
2024-08-22 00:06:16 +0200

    remove old chain

--- contrib/auto_gdb/raptoreum_dbg.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# use testnet settings,  if you need mainnet,  use ~/.ravencashcore/ravencashd.pid file instead
-ravencash_pid=$(<~/.ravencashcore/testnet3/ravencashd.pid)
-sudo gdb -batch -ex "source debug.gdb" ravencashd ${ravencash_pid}
